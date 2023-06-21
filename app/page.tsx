a26af3ba24fb4fabbd28cacf13af02bd77542540
Turjo99 <imturjo27@gmail.com>
2023-06-21 10:46:56 +0600

    removed default css and installed tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
-  return <div className="">hello bnb</div>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
