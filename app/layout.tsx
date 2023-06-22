6968888f3ecec942517e03fea0db31f6aa6ddf12
Turjo99 <imturjo27@gmail.com>
2023-06-22 12:40:08 +0600

    resolved runtime error on refreshing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar></Navbar>
+        <ClientOnly>
+          <Navbar></Navbar>
+        </ClientOnly>
         {children}
       </body>
     </html>
