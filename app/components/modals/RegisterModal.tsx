892b53dc1faa59f4d7103e2725ab771c079f8a77
Turjo99 <imturjo27@gmail.com>
2023-06-26 21:21:51 +0600

    created necessary functions for signup post api and modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,5 +1,35 @@
 "use client";
+
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import axios from "axios";
+import { useState } from "react";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+
 const RegisterModal = () => {
+  const registerModal = useRegisterModal();
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(false);
+    axios
+      .post("/api/register", data)
+      .then(() => {
+        registerModal.onClose;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return <div className=""></div>;
 };
 export default RegisterModal;
