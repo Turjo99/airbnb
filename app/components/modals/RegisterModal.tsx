d587cda6501f0280584048367826c5e34eeb8f0e
Turjo99 <imturjo27@gmail.com>
2023-07-04 21:36:29 +0600

    added footer content of modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -8,6 +8,8 @@ import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../Input/Input";
 import { toast } from "react-hot-toast";
+import Button from "../Button";
+import { FcGoogle } from "react-icons/fc";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -68,6 +70,17 @@ const RegisterModal = () => {
       ></Input>
     </div>
   );
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+    </div>
+  );
   return (
     <div className="">
       <Modal
@@ -78,6 +91,7 @@ const RegisterModal = () => {
         onClose={registerModal.onClose}
         onSubmit={handleSubmit(onSubmit)}
         body={bodyContent}
+        footer={footerContent}
       />
     </div>
   );
