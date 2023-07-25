65492631428feae6064867fa857fed362ff62719
Turjo99 <imturjo27@gmail.com>
2023-07-25 21:16:49 +0600

    used getSession on server component

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -4,3 +4,13 @@ import { getServerSession } from "next-auth";
 export async function getSession() {
   return await getServerSession(authOptions);
 }
+export default async function getCurrentUser() {
+  try {
+    const session = await getSession();
+    if (!session?.user?.email) {
+      return null;
+    }
+  } catch (error: any) {
+    return null;
+  }
+}
